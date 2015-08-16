891ac774a39dbfd84bcca6849b6b387fd9ba72e3
Peter T. Brown <peter@flippyhead.com>
2015-08-16 10:55:47 -0700

    more handy tools and audio stuff; calibration mode

---
+++ audio.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+say -o ./audio/T00.WAV –data-format=LEF32@8000 "Tier 0 towards, 5 meters."
+say -o ./audio/T01.WAV –data-format=LEF32@8000 "Tier 1 towards, 10 meters."
+say -o ./audio/T02.WAV –data-format=LEF32@8000 "Tier 2 towards, 20 meters."
+say -o ./audio/T03.WAV –data-format=LEF32@8000 "Tier 3 towards, 40 meters."
+say -o ./audio/T04.WAV –data-format=LEF32@8000 "Tier 4 towards, 80 meters."
+say -o ./audio/T05.WAV –data-format=LEF32@8000 "Tier 5 towards, 160 meters."
+say -o ./audio/T06.WAV –data-format=LEF32@8000 "Tier 6 towards, 320 meters."
+say -o ./audio/T07.WAV –data-format=LEF32@8000 "Tier 7 towards, 640 meters."
+say -o ./audio/T08.WAV –data-format=LEF32@8000 "Tier 8 towards, 1280 meters."
+say -o ./audio/T09.WAV –data-format=LEF32@8000 "File 9, not used."
+
+say -o ./audio/T10.WAV –data-format=LEF32@8000 "Tier 0 away, 5 meters."
+say -o ./audio/T11.WAV –data-format=LEF32@8000 "Tier 1 away, 10 meters."
+say -o ./audio/T12.WAV –data-format=LEF32@8000 "Tier 2 away, 20 meters."
+say -o ./audio/T13.WAV –data-format=LEF32@8000 "Tier 3 away, 40 meters."
+say -o ./audio/T14.WAV –data-format=LEF32@8000 "Tier 4 away, 80 meters."
+say -o ./audio/T15.WAV –data-format=LEF32@8000 "Tier 5 away, 160 meters."
+say -o ./audio/T16.WAV –data-format=LEF32@8000 "Tier 6 away, 320 meters."
+say -o ./audio/T17.WAV –data-format=LEF32@8000 "Tier 7 away, 640 meters."
+say -o ./audio/T18.WAV –data-format=LEF32@8000 "Tier 8 away, 1280 meters."
+say -o ./audio/T19.WAV –data-format=LEF32@8000 "File 19, not used."
+
+say -o ./audio/T20.WAV –data-format=LEF32@8000 "Startup."
+say -o ./audio/T21.WAV –data-format=LEF32@8000 "Other."
