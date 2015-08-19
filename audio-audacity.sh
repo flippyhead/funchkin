cd13d72701ce16f79d2862688a9b5e4f1c589c95
Peter T. Brown <peter@flippyhead.com>
2015-08-19 15:30:20 -0700

    add script to convert audacity files

---
+++ audio-audacity.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+# Distance Tiers towards
+sox ./audio/tmp/T00.wav -r 16000 ./audio/T00.wav
+sox ./audio/tmp/T01.wav -r 16000 ./audio/T01.wav
+sox ./audio/tmp/T02.wav -r 16000 ./audio/T02.wav
+sox ./audio/tmp/T03.wav -r 16000 ./audio/T03.wav
+sox ./audio/tmp/T04.wav -r 16000 ./audio/T04.wav
+sox ./audio/tmp/T05.wav -r 16000 ./audio/T05.wav
+sox ./audio/tmp/T06.wav -r 16000 ./audio/T06.wav
+# sox ./audio/tmp/T07.wav -r 16000 ./audio/T07.wav
+# sox ./audio/tmp/T08.wav -r 16000 ./audio/T08.wav
+# sox ./audio/tmp/T09.wav -r 16000 ./audio/T09.wav
+
+
+# Distance Tiers away
+sox ./audio/tmp/T10.wav -r 16000 ./audio/T10.wav
+sox ./audio/tmp/T11.wav -r 16000 ./audio/T11.wav
+sox ./audio/tmp/T12.wav -r 16000 ./audio/T12.wav
+sox ./audio/tmp/T13.wav -r 16000 ./audio/T13.wav
+sox ./audio/tmp/T14.wav -r 16000 ./audio/T14.wav
+sox ./audio/tmp/T15.wav -r 16000 ./audio/T15.wav
+sox ./audio/tmp/T16.wav -r 16000 ./audio/T16.wav
+sox ./audio/tmp/T17.wav -r 16000 ./audio/T17.wav
+sox ./audio/tmp/T18.wav -r 16000 ./audio/T18.wav
+sox ./audio/tmp/T19.wav -r 16000 ./audio/T19.wav
+
+
+# Extras
+sox ./audio/tmp/T20.wav -r 16000 ./audio/T20.wav
+sox ./audio/tmp/T21.wav -r 16000 ./audio/T21.wav
